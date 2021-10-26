da433044827b660d372b88d833a5e9e41b3e39aa
CgtzNoemi <89617943+CgtzNoemi@users.noreply.github.com>
2021-10-26 17:47:54 -0500

    Update proyecto.js

--- proyecto.js
+++ proyecto.js
@@ -115,3 +115,52 @@ function parar(){
     clearInterval(iniciar_temporizador);
     iniciar_temporizador = undefined;
 }
+
+function enviarTexto(){
+    var texto = document.getElementById("InputText2").value;
+    var texto2 = document.getElementById("InputText").value = texto;
+    
+    if(texto === ''){
+        alert('Escribe el nombre de la tarea!!');
+    }
+
+
+    if(texto2 != ''){
+        document.getElementById("botonTexto").value = "cambiar";
+        document.getElementById("InputText2").value = "";
+    }
+    
+}
+let cerrar = document.querySelectorAll(".close")[0];
+let abrir = document.querySelectorAll(".cambio")[0];
+let modal = document.querySelectorAll(".modal")[0];
+let modalC = document.querySelectorAll(".modal-container")[0];
+
+abrir.addEventListener("click", function(e){
+    e.preventDefault();
+    modalC.style.opacity = "1";
+    modalC.style.visibility = "visible";
+    modal.classList.toggle("modal-close");
+
+});
+
+cerrar.addEventListener("click", function(){
+    modal.classList.toggle("modal-close");
+    setTimeout(function(){
+        modalC.style.opacity = "0";
+        modalC.style.visibility = "hidden";
+    },600)
+});
+
+window.addEventListener("click", function(e){
+    console.log(e.target)
+    if(e.target == modalC){
+        modal.classList.toggle("modal-close");
+        setTimeout(function(){
+            modalC.style.opacity = "0";
+            modalC.style.visibility = "hidden";
+        },600)
+    }
+})
+
+
